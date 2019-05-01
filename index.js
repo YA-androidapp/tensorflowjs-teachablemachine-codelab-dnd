8ea3ee17f47f6d91eaa78dfcd8b446034dfcd8bd
YA <YA-androidapp@users.noreply.github.com>
2019-05-02 00:44:17 +0900

    Update DnD

--- index.js
+++ index.js
@@ -8,62 +8,6 @@ const webcamElement = document.getElementById('webcam');
 let net;
 var classLabels = new Array();
 
-// Reads an image from the webcam and associates it with a specific class
-// index.
-function addExample(classLabel) {
-  // console.log('classLabel: ' + classLabel);
-
-  // Get the intermediate activation of MobileNet 'conv_preds' and pass that
-  // to the KNN classifier.
-  const activation = net.infer(webcamElement, 'conv_preds');
-
-  // Pass the intermediate activation to the classifier.
-  classifier.addExample(activation, classLabel);
-
-  let buttonElement = document.getElementById(classPrefix + classLabel);
-  let spt = buttonElement.innerText.split(splitStr)
-  buttonElement.innerText = spt[0] + splitStr + ((spt[1]) == undefined ? 1 : parseInt((spt[1]).trim()) + 1);
-}
-
-async function app() {
-  document.getElementById('message').innerText = 'Loading mobilenet..';
-
-  // Load the model.
-  net = await mobilenet.load();
-  document.getElementById('message').innerText = 'Sucessfully loaded model';
-
-  await setupWebcam();
-
-
-  // When clicking a button, add a class.
-  document.getElementById('add-class-button').addEventListener('click', () => {
-    let classLabel = document.getElementById('class-name').value;
-    document.getElementById('buttons').innerHTML += ' <button id="' + classPrefix + classLabel + '" onclick="addExample(\'' + classLabel + '\')">Add ' + classLabel + '</button>';
-    document.getElementById('canvases').innerHTML += ' <div style="border:#000 1px solid;float:left;width:256px;height:256px;"><canvas id="' + canvasPrefix + classLabel + '" style="height:' + img_height + 'px;width:' + img_width + 'px;" title="' + classLabel + '" ondrop="initCanvas(\'' + canvasPrefix + classLabel + '\')"></canvas></div>';
-    classLabels.push(classLabel);
-    initCanvas(canvasPrefix + classLabel);
-
-    document.getElementById('class-name').value = '';
-  });
-
-  while (true) {
-    if (classifier.getNumClasses() > 0) {
-      // Get the activation from mobilenet from the webcam.
-      const activation = net.infer(webcamElement, 'conv_preds');
-      // Get the most likely class and confidences from the classifier module.
-      const result = await classifier.predictClass(activation);
-
-      console.log(result);
-      document.getElementById('console').innerText = `
-        prediction: ${result.label}\t
-        probability: ${result.confidences[result.label]}
-      `;
-    }
-
-    await tf.nextFrame();
-  }
-}
-
 async function setupWebcam() {
   return new Promise((resolve, reject) => {
     const navigatorAny = navigator;
@@ -85,13 +29,90 @@ async function setupWebcam() {
   });
 }
 
-function initCanvas(canvasId) {
+// Reads an image from the webcam and associates it with a specific class
+// index.
+function addExampleVideo(_classLabel) {
+  // console.log('classLabel: ' + _classLabel);
+
+  // Get the intermediate activation of MobileNet 'conv_preds' and pass that
+  // to the KNN classifier.
+  const activation = net.infer(webcamElement, 'conv_preds');
+
+  // Pass the intermediate activation to the classifier.
+  classifier.addExample(activation, _classLabel);
+
+  let buttonElement = document.getElementById(classPrefix + _classLabel);
+  let spt = buttonElement.innerText.split(splitStr)
+  buttonElement.innerText = spt[0] + splitStr + ((spt[1]) == undefined ? 1 : parseInt((spt[1]).trim()) + 1);
+}
+
+function addExampleImage(_classLabel) {
+  // console.log('classLabel: ' + _classLabel);
+
+  var canvasId = canvasPrefix + _classLabel
+  var canvas = document.getElementById(canvasId);
+
+  // Get the intermediate activation of MobileNet 'conv_preds' and pass that
+  // to the KNN classifier.
+  const activation = net.infer(canvas, 'conv_preds');
+
+  // Pass the intermediate activation to the classifier.
+  classifier.addExample(activation, _classLabel);
+
+  let buttonElement = document.getElementById(classPrefix + _classLabel);
+  let spt = buttonElement.innerText.split(splitStr)
+  buttonElement.innerText = spt[0] + splitStr + ((spt[1]) == undefined ? 1 : parseInt((spt[1]).trim()) + 1);
+}
+
+async function app() {
+  document.getElementById('message').innerText = 'Loading mobilenet..';
+
+  // Load the model.
+  net = await mobilenet.load();
+  document.getElementById('message').innerText = 'Sucessfully loaded model';
+
+  await setupWebcam();
+
+  // When clicking a button, add a class.
+  document.getElementById('add-class-button').addEventListener('click', () => {
+    let classLabel = document.getElementById('class-name').value;
+    document.getElementById('buttons').innerHTML += ' <button id="' + classPrefix + classLabel + '" onclick="addExampleVideo(\'' + classLabel + '\')">Add ' + classLabel + '</button>';
+    document.getElementById('canvases').innerHTML += ' <div style="border:#000 1px solid;float:left;width:256px;height:256px;"><canvas id="' + canvasPrefix + classLabel + '" style="height:' + img_height + 'px;width:' + img_width + 'px;" title="' + classLabel + '"></canvas></div>';
+    classLabels.push(classLabel);
+    initCanvasTrain(classLabel);
+
+    document.getElementById('class-name').value = '';
+  });
+
+  while (true) {
+    if (classifier.getNumClasses() > 0) {
+      // Get the activation from mobilenet from the webcam.
+      const activation = net.infer(webcamElement, 'conv_preds');
+      // Get the most likely class and confidences from the classifier module.
+      const result = await classifier.predictClass(activation);
+
+      // console.log(result);
+      document.getElementById('console').innerText = `
+        prediction: ${result.label}\t
+        probability: ${result.confidences[result.label]}
+      `;
+    }
+
+    await tf.nextFrame();
+  }
+}
+
+function initCanvasTrain(_classLabel) {
+  var canvasId = canvasPrefix + _classLabel
   var canvas = document.getElementById(canvasId);
   var ctx = canvas.getContext('2d');
   var render = function(image) {
     canvas.height = img_height;
     canvas.width = img_width;
     ctx.drawImage(image, 0, 0, image.width, image.height, 0, 0, img_width, img_height);
+    console.log(image)
+
+    addExampleImage(_classLabel)
   };
   canvas.addEventListener("drop", function(e) {
     e.preventDefault();
